bfdeece7115b0ebf6a716041ee63c2cd4bd4842a
zysoong <ziyang.song@gmx.de>
2023-09-10 14:16:29 +0200

    Reformat frontend codes

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { RecoilRoot } from "recoil";
+import {RecoilRoot} from "recoil";
 import recoilPersist from "recoil-persist";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const { RecoilPersist, updateState } = recoilPersist([], {
-  key: "recoil-persist",
-  storage: sessionStorage,
+const {RecoilPersist, updateState} = recoilPersist([], {
+    key: "recoil-persist",
+    storage: sessionStorage,
 });
 
 ReactDOM.render(
-  <RecoilRoot initializeState={updateState}>
-    <RecoilPersist />
-    <App />
-  </RecoilRoot>,
-  document.getElementById("root")
+    <RecoilRoot initializeState={updateState}>
+        <RecoilPersist/>
+        <App/>
+    </RecoilRoot>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
