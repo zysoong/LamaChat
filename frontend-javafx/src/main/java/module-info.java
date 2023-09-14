da59f879eb25e25d81e0123d71e2092120980007
zysoong <ziyang.song@gmx.de>
2023-09-14 14:15:36 +0200

    Add login view with security functionality

--- frontend-javafx/src/main/java/module-info.java
+++ frontend-javafx/src/main/java/module-info.java
@@ -1,10 +1,20 @@
 module com.example.frontendjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires java.net.http;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
 
-    opens com.example.frontendjavafx to javafx.fxml;
+    opens com.example.frontendjavafx;
     exports com.example.frontendjavafx;
+
+    opens com.example.frontendjavafx.security;
+    exports com.example.frontendjavafx.security;
+
+    opens com.example.frontendjavafx.controller;
+    exports com.example.frontendjavafx.controller;
+
+
 }
