91ff102e9bc89e61de0dc37114d9ef7d1bade9aa
zysoong <ziyang.song@gmx.de>
2023-09-22 16:12:01 +0200

    Fix bug on chat view (reflector error on the gui)

--- frontend-javafx/src/main/java/com/example/frontendjavafx/model/ChatSession.java
+++ frontend-javafx/src/main/java/com/example/frontendjavafx/model/ChatSession.java
@@ -5,15 +5,9 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@AllArgsConstructor
-public class ChatSession{
-    private String chatSessionId;
-    private String uniqueSessionIdentifier;
-    private List<ChatMessage> chat_messages;
-
-    public String chatSessionId(){return this.chatSessionId; }
-    public String uniqueSessionIdentifier(){return this.uniqueSessionIdentifier; }
-    public List<ChatMessage> chat_messages(){return this.chat_messages; }
-
-
+public record ChatSession(
+        String chatSessionId,
+        String uniqueSessionIdentifier,
+        List<ChatMessage> chat_messages
+) {
 }
