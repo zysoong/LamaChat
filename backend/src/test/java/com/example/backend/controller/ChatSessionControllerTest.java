fd7fe1c9c41fe2af4b8c55d19c5683a40010b82c
zysoong <ziyang.song@gmx.de>
2023-10-05 11:52:00 +0200

    Complete integration test for adding new chat session

--- backend/src/test/java/com/example/backend/controller/ChatSessionControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/ChatSessionControllerTest.java
@@ -125,13 +125,98 @@ public class ChatSessionControllerTest {
     @DirtiesContext
     @WithMockUser
     void addChatSession_whenChatSessionExistsAndUserExists_thenDoNothing() throws Exception {
-        
+
+        AppUser user1 = new AppUser(
+                null,
+                "user1",
+                "123456",
+                AppUserRole.USER,
+                new ArrayList<>(),
+                false,
+                "", ""
+        );
+
+        AppUser user2 = new AppUser(
+                null,
+                "user2",
+                "123456",
+                AppUserRole.USER,
+                new ArrayList<>(),
+                false,
+                "", ""
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user1)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user2)))
+                .andExpect(status().isCreated());
+
+        AppUser savedUser1 =
+                appUserRepository
+                        .findAppUserByUserName("user1")
+                        .orElseThrow(() ->
+                                new NoSuchElementException("Error in ChatSession controller test. user1 not found")
+                        );
+
+        AppUser savedUser2 =
+                appUserRepository
+                        .findAppUserByUserName("user2")
+                        .orElseThrow(() ->
+                                new NoSuchElementException("Error in ChatSession controller test. user2 not found")
+                        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/chatsessions/" +
+                        savedUser1.userId() + "/" + savedUser2.userId())
+                )
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/chatsessions/" +
+                        savedUser1.userId() + "/" + savedUser2.userId())
+                )
+                .andExpect(status().isCreated());
+
+        AppUser user1AfterCreation =
+                appUserRepository
+                        .findAppUserByUserName("user1")
+                        .orElseThrow(() ->
+                                new NoSuchElementException("Error in ChatSession controller test. user1 not found")
+                        );
+
+        AppUser user2AfterCreation =
+                appUserRepository
+                        .findAppUserByUserName("user2")
+                        .orElseThrow(() ->
+                                new NoSuchElementException("Error in ChatSession controller test. user2 not found")
+                        );
+
+        String uniqueSessionId = generateSessionUniqueIdentifier(savedUser1.userId(), savedUser2.userId());
+
+        assertEquals(user1AfterCreation.chat_sessions().get(0).uniqueSessionIdentifier(),
+                uniqueSessionId);
+
+        assertEquals(user2AfterCreation.chat_sessions().get(0).uniqueSessionIdentifier(),
+                uniqueSessionId);
+
+        chatSessionRepository
+                .findChatSessionByUniqueSessionIdentifier(uniqueSessionId)
+                .orElseThrow(() -> new NoSuchElementException("Error in ChatSession controller test. chat session not found"));
+
     }
 
     @Test
     @DirtiesContext
     @WithMockUser
-    void addChatSession_whenUserDoesNotExist_thenThrowNoSuchElementException() throws Exception {
+    void addChatSession_whenUserDoesNotExist_thenReturnBadRequest() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/chatsessions/" +
+                        "123" + "/" + "456")
+                )
+                .andExpect(status().isBadRequest());
 
     }
 
@@ -139,7 +224,7 @@ public class ChatSessionControllerTest {
     @DirtiesContext
     @WithMockUser
     void addMessageToChatSession_whenNoChatSessionExists_thenAddNewSessionAndAddMessage() throws Exception {
-
+        
     }
 
     @Test
