c4a89a1dcc6e31f3659872a8f7f9285b24a47bcb
zysoong <ziyang.song@gmx.de>
2023-10-13 18:18:26 +0200

    Add robot icon after every chat partner (debugging)

--- frontend-react/src/chat/Chat.js
+++ frontend-react/src/chat/Chat.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState} from "react";
-import {Button, notification} from "antd";
+import { Button, notification } from "antd";
+
 import {
     getMe, findUserByUserName, findOrAddChatSessionByParticipantIds, getMyContacts, logout,
 } from "../util/ApiUtil";
@@ -286,7 +287,12 @@ const Chat = (props) => {
 
                                     <img id={partner.userId} src={imageMap[partner.userId]} alt=""/>
                                     <div className="meta">
-                                        <p className="name">{partner.userName}</p>
+                                        <p className="name">
+                                            {partner.userName}
+                                            <i className="robot-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a2 2 0 0 1 2 2c0 .74-.4 1.39-1 1.73V7h1a7 7 0 0 1 7 7h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1a7 7 0 0 1 7-7h1V5.73c-.6-.34-1-.99-1-1.73a2 2 0 0 1 2-2M7.5 13A2.5 2.5 0 0 0 5 15.5A2.5 2.5 0 0 0 7.5 18a2.5 2.5 0 0 0 2.5-2.5A2.5 2.5 0 0 0 7.5 13m9 0a2.5 2.5 0 0 0-2.5 2.5a2.5 2.5 0 0 0 2.5 2.5a2.5 2.5 0 0 0 2.5-2.5a2.5 2.5 0 0 0-2.5-2.5Z"/></svg>
+                                            </i>
+                                        </p>
                                     </div>
                                 </div>
                             </li>
